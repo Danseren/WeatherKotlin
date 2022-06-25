1e12ee5a28ea5771c03c823b4ce6d83b5c916f04
Den <etlendo@mail.ru>
2022-06-25 18:52:05 +0500

    Урок 1 | Сформировал data class с двумя свойствами и вывел их на экран приложения в extView при нажатии на кнопку

--- app/src/main/java/geekbrains/android/weatherkotlin/MainActivity.kt
+++ app/src/main/java/geekbrains/android/weatherkotlin/MainActivity.kt
@@ -5,15 +5,20 @@ import android.os.Bundle
 import android.util.Log
 import android.view.View
 import android.widget.Button
+import android.widget.TextView
 import geekbrains.android.weatherkotlin.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val tv_start:TextView = findViewById<TextView>(R.id.tv_start)
+        //findViewById<TextView>(R.id.tv_start)
         findViewById<Button>(R.id.btn_go).setOnClickListener(object : View.OnClickListener {
             override fun onClick(p0: View?) {
                 Log.d("My_Log", "It's alive!")
+                val first_student = OneOfUs("Andrei", 33)
+                tv_start.setText(first_student.toString())
             }
 
         })
@@ -27,7 +32,7 @@ class MainActivity : AppCompatActivity() {
     Перевести MainActivity на Kotlin.
     Добавить кнопку в разметку и повесить на неё clickListener в Activity.
 Потренироваться в создании классов и функций, описанных в уроке, и убедиться, что всё работает. Например, создать тестовое приложение:
-Сформировать data class с двумя свойствами и вывести их на экран приложения.
+    Сформировать data class с двумя свойствами и вывести их на экран приложения.
 Создать Object. В Object вызвать copy и вывести значения скопированного класса на экран.
 Вывести значения из разных циклов в консоль, используя примеры из методических материалов.
 Изучить API погоды «Яндекса», посмотреть примеры и зарегистрироваться в качестве разработчика, получить свой ключ разработчика.
---
+++ app/src/main/java/geekbrains/android/weatherkotlin/OneOfUs.kt
@@ -0,0 +1,4 @@
+package geekbrains.android.weatherkotlin
+
+data class OneOfUs(val name:String, val birthYear:Int) {
+}
