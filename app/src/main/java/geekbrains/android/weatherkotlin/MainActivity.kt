64ab8cb85e69d0498735f8762e25f7c4935458e1
Den <etlendo@mail.ru>
2022-06-25 20:34:31 +0500

    Урок 1 | Создал новый проект уже на Котлине

--- app/src/main/java/geekbrains/android/weatherkotlin/MainActivity.kt
+++ app/src/main/java/geekbrains/android/weatherkotlin/MainActivity.kt
@@ -6,13 +6,14 @@ import android.util.Log
 import android.view.View
 import android.widget.Button
 import android.widget.TextView
-import geekbrains.android.weatherkotlin.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
         val tv_start:TextView = findViewById<TextView>(R.id.tv_start)
+
         findViewById<Button>(R.id.btn_go).setOnClickListener(object : View.OnClickListener {
             override fun onClick(p0: View?) {
                 Log.d("My_Log", "It's alive!")
@@ -20,10 +21,12 @@ class MainActivity : AppCompatActivity() {
                 val second_student = first_student.copy()
                 //tv_start.setText("First student: " + first_student.toString() + "\nSecond student (копия): " + second_student.toString()) Интересно студия предлагает вот это все заменить. Непривычно
                 tv_start.setText("First student: $first_student\nSecond student (копия): $second_student")
+
             }
 
         })
     }
+
 }
 
 /*
@@ -34,7 +37,7 @@ class MainActivity : AppCompatActivity() {
     Добавить кнопку в разметку и повесить на неё clickListener в Activity.
 Потренироваться в создании классов и функций, описанных в уроке, и убедиться, что всё работает. Например, создать тестовое приложение:
     Сформировать data class с двумя свойствами и вывести их на экран приложения.
-Создать объект класса из предыдущего пункта, в этом бъекте вызвать copy и вывести значения скопированного класса на экран (Создать Object. В Object вызвать copy и вывести значения скопированного класса на экран).
+    Создать объект класса из предыдущего пункта, в этом бъекте вызвать copy и вывести значения скопированного класса на экран (Создать Object. В Object вызвать copy и вывести значения скопированного класса на экран).
 Вывести значения из разных циклов в консоль, используя примеры из методических материалов.
 Изучить API погоды «Яндекса», посмотреть примеры и зарегистрироваться в качестве разработчика, получить свой ключ разработчика.
 Изучить API КиноПоиска и зарегистрироваться в качестве разработчика, подключиться к API.
